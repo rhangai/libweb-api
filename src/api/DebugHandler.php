c9cdebe17154939f23081393320700bb66b9fdd5
renanhangai <hangai.renan@gmail.com>
2017-12-07 08:41:11 -0200

    Fixed debug handler

--- src/api/DebugHandler.php
+++ src/api/DebugHandler.php
@@ -10,15 +10,17 @@ class DebugHandler {
 	public function dispatchRequest( $req, $res ) {
 		$uri = '/'.implode( "/", array_filter( explode( "/", $req->relativeUri() ) ) );
 		if ( $uri === '/_debug/debug.js' ) {
-			Debug::dumpJs( $req->base() );
+			$res->raw(function() use( $req ) { Debug::dumpJs( $req->base() ); });
 		} else if ( $uri === '/_debug/debug.css' ) {
-			Debug::dumpCss( $req->base() );
+			$res->raw(function() use( $req ) { Debug::dumpCss( $req->base() ); });
 		} else if ( $uri === '/_debug/handler.json' ) {
-			Debug::dumpHandler();
+			$res->raw(function() { Debug::dumpHandler(); });
 		} else if ( self::_strStartsWith( $uri, "/_debug/fontawesome-webfont" ) ) {
-			Debug::dumpFontAwesome( $uri );
+			$res->raw(function() use( $uri ) { Debug::dumpFontAwesome( $uri ); });
+		} else {
+			$res->code( 404 );
+			$res->raw( "Not Found" );
 		}
-		exit;
 	}
 
 	private static function _strStartsWith( $str, $other ) {

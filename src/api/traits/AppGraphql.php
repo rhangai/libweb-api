e06a911b4aad67f91db0ab628e10188ed0f0cf92
renanhangai <hangai.renan@gmail.com>
2018-07-28 22:59:09 -0300

    chore: Added graphql trait to help generate graphql APIs

---
+++ src/api/traits/AppGraphql.php
@@ -0,0 +1,44 @@
+<?php
+namespace libweb\api\traits;
+
+/**
+ * Enables graphql on the current App
+ */
+trait AppGraphql {
+
+	/**
+	 * Add GraphQL handlers for the current uri
+	 * 
+	 * @param string $uri The path URI for the handler
+	 * @param $options["schema"] The schema to run the graphql
+	 * @param $options["context"] The context for the graphql
+	 * @param $options["rootValue"] The value to pass to the root
+	 */
+	public function graphql( $uri, array $options ) {
+		$handler = function( $req, $res ) use ( $options ) {
+			$schema = $options["schema"];
+			if ( is_callable( $schema ) )
+				$schema = call_user_func( $schema, $req, $res );
+
+			$context = @$options["context"];
+			if ( is_callable( $context ) )
+				$context = call_user_func( $context, $req, $res );
+	
+	
+			$query     = $req->getParam( "query" );
+			$variables = $req->getParam( "variables" );
+			$result = \GraphQL\GraphQL::executeQuery( $schema, $query, @$options["rootValue"], $context, (array) $variables );
+
+			$flags = 0;
+			if ( $this->getContainer()["settings"]["displayErrorDetails"] )
+				$flags |= \GraphQL\Error\Debug::INCLUDE_DEBUG_MESSAGE;
+			$responseData = $result->toArray( $flags );
+			$responseData["status"] = $result->errors ? "error" : "success";
+			return $res->withJson( $responseData );
+		};
+
+		$this->get(  $uri, $handler );
+		$this->post( $uri, $handler );
+		return $this;
+	}
+}
